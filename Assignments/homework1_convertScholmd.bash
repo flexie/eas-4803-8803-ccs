dc6356c2bad836950d562e7b7ef8c96ff2ae4f88
Ziyi (Francis) Yin <ziyi.yin@gatech.edu>
2022-01-15 13:14:40 -0500

    Deployed ae310fc with MkDocs version: 1.1.2

---
+++ Assignments/homework1_convertScholmd.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+PATH=~/local/bin:/opt/SLIM/ScholarlyMarkdown/bin:/usr/texbin:/usr/local/bin:/opt/local/bin:/sw/bin:/usr/bin:/bin:/usr/sbin:/sbin
+scholdoc --css='https://slimgroup.slim.gatech.edu/ScholMD/core/slimweb-scholmd-core-v0.1-latest.css' --mathjax='https://slimgroup.slim.gatech.edu/MathJax/MathJax.js?config=TeX-AMS_HTML-full' --to=html --default-image-extension=png --citeproc "homework1.md" "/opt/SLIM/ScholarlyMarkdown/configs/default_csl.yaml" -o "homework1.html" 2>&1
