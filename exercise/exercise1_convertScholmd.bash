e7c52f8e1768f1d0e2e97520cfe0e60e6b432fa9
Felix Herrmann <felix.herrmann@gatech.edu>
2022-01-16 15:22:22 -0500

    Deployed df27bc8 with MkDocs version: 1.2.3

--- exercise/exercise1_convertScholmd.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-PATH=~/local/bin:/opt/SLIM/ScholarlyMarkdown/bin:/usr/texbin:/usr/local/bin:/opt/local/bin:/sw/bin:/usr/bin:/bin:/usr/sbin:/sbin
-scholdoc --css='https://slimgroup.slim.gatech.edu/ScholMD/core/slimweb-scholmd-core-v0.1-latest.css' --mathjax='https://slimgroup.slim.gatech.edu/MathJax/MathJax.js?config=TeX-AMS_HTML-full' --to=html --default-image-extension=png --citeproc "exercise1.md" "/opt/SLIM/ScholarlyMarkdown/configs/default_csl.yaml" -o "exercise1.html" 2>&1
